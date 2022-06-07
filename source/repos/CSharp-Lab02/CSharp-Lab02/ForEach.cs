7c575bd1a64da91f846381160bf7ea605e57731a
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-06-07 17:54:17 +0700

    Lab2

---
+++ source/repos/CSharp-Lab02/CSharp-Lab02/ForEach.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+class ForEach
+{
+    static void Main(string[] args)
+    {
+        DateTime now = DateTime.Now;
+        Random rand = new Random((int)now.Millisecond);
+        int[] Arr = new int[10];
+        for (int x = 0; x < Arr.Length; ++x)
+        {
+            Arr[x] = rand.Next() % 100;
+        }
+        int Total = 0;
+        Console.Write("Array values are ");
+        foreach (int val in Arr)
+        {
+            Total += val;
+            Console.Write(val + ", ");
+        }
+        Console.WriteLine("\nAnd the average is {0,0:F1}",
+                          (double)Total / (double)Arr.Length);
+        Console.ReadLine();
+    }
+}*/
+
---
+++ source/repos/CSharp-Lab02/CSharp-Lab02/ObjectArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Employee
+{
+    private int empID;
+
+    // constructor
+    public Employee(int empID)
+    {
+        this.empID = empID;
+    }
+    public override string ToString()
+    {
+        return empID.ToString();
+    }
+}
+class ObjectArray
+{
+    public void Run()
+    {
+        int[] intArray;
+        Employee[] empArray;
+        intArray = new int[5];
+        empArray = new Employee[3];
+
+        // populate the array
+        for (int i = 0; i < empArray.Length; i++)
+        {
+            empArray[i] = new Employee(i + 5);
+        }
+
+        Console.WriteLine("The int array...");
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            Console.WriteLine(intArray[i].ToString());
+        }
+
+        Console.WriteLine("\nThe employee array...");
+        for (int i = 0; i < empArray.Length; i++)
+        {
+            Console.WriteLine(empArray[i].ToString());
+        }
+    }
+    static void Main(string[] args)
+    {
+        ObjectArray arr = new ObjectArray();
+        arr.Run();
+        Console.ReadLine();
+    }
+}
+
